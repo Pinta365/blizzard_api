2fc40bf84e63a9cf22fd2e62f3d0e8fbaac7e7f9
Pinta365 <anders@billfors.com>
2023-09-12 20:42:56 +0200

    default export

--- mod.ts
+++ mod.ts
@@ -2,9 +2,11 @@ import { authenticate, setup } from "./src/shared/index.ts";
 import * as wow from "./src/wow/index.ts";
 import * as wowClassic from "./src/wow_classic/index.ts";
 
-export const blizzardAPI = {
+const blizzardAPI = {
     setup,
     authenticate,
     wow,
     wowClassic,
 };
+
+export default blizzardAPI;
