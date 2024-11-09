da55e0233f9725f309d373e2325c9b150c344f2d
Pinta365 <hello@pinta.land>
2024-11-09 16:03:41 +0100

    export errors

--- mod.ts
+++ mod.ts
@@ -2,5 +2,6 @@ import { authenticate, setup } from "./src/shared/index.ts";
 import * as wow from "./src/wow/index.ts";
 import * as hearthstone from "./src/hearthstone/index.ts";
 import * as sc2 from "./src/starcraft2/index.ts";
+import * as errors from "./src/shared/errors.ts"
 
-export { authenticate, hearthstone, sc2, setup, wow };
+export { authenticate, hearthstone, sc2, setup, wow, errors};
